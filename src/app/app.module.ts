e84501f39b48a957440bc530498819d9222b2581
pierabellelli <86509447+pierabellelli@users.noreply.github.com>
2021-11-05 19:44:32 +0100

    1st commit esercizio 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './user/user-container/user-container.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserSingleComponent } from './user/user-single/user-single.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { OrderContainerComponent } from './order/order-container/order-container.component';
+import { OrderListComponent } from './order/order-list/order-list.component';
+import { OrderSingleComponent } from './order/order-single/order-single.component';
+import { OrderDetailComponent } from './order/order-detail/order-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    UserListComponent,
+    UserSingleComponent,
+    UserDetailComponent,
+    OrderContainerComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    OrderDetailComponent
   ],
   imports: [
     BrowserModule,
