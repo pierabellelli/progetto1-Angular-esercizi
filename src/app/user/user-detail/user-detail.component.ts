4252416a9ba56b7f8bae58a11100b73a36ee3c93
pierabellelli <86509447+pierabellelli@users.noreply.github.com>
2021-11-09 16:29:42 +0100

    2nd

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Gender, IUser, Role } from 'src/app/model/models';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,6 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
+    obj: IUser = {
+        id: 3487,
+           name: 'Mario',
+           surname: 'Rossi',
+           age: 25,
+           dateOfBirth: '1995-14-12',
+           address: {
+               city: 'Roma',
+               street: 'Via roma 10',
+               postalCode: '00100'
+           },
+           role: Role.staff, 
+           username: 'MarioRossi80',
+           profilePhotoUrl: 'https://bit.ly/3yRngEP',
+           companies: [
+         {
+                   id: 148979,
+                   name: 'Develhope',
+                   description: 'La migliore',
+                   location: {
+                       city: 'Palermo',
+                       street: 'Via Libertà 58',
+                       postalCode: '90139'
+                   }
+               },
+               {
+                   id: 123123,
+                   name: 'Apple',
+                   description: 'E insomma...',
+                   location: {
+                       city: 'Cupertino',
+                       street: 'One Apple Park Way',
+                       postalCode: '95014'
+                   }
+               }
+        ],
+           gender: Gender.male,
+       }
     
   constructor() { }
 
